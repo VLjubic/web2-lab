ee2334b38c2989612dce01b6d2dab4738d8f1107
Vlaho Ljubic <vlaho.ljubic@fer.hr>
2022-10-30 22:07:24 +0100

    database url

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(
+  "postgres://vlaho:DFq3t7ABOko0IQjvghYiu9KyqgfXtESG@dpg-cdfe07pgp3juhhuvok30-a/web2lab_rb4o"
+);
 
 const models: any = {};
 
